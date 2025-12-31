b9e6df2ad54b582dd17b54658df66fbb00d0fa6d
Rayanne Melo <rayanne.melo@nibo.com.br>
2025-12-31 06:22:56 -0300

    feat: organizando pacotes back para web-api

---
+++ web-api/src/main/java/br/ufc/crateus/pi2/botservice/repositories/UserRepository.java
@@ -0,0 +1,16 @@
+package br.ufc.crateus.pi2.botservice.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.ufc.crateus.pi2.botservice.models.User;
+
+public interface UserRepository extends JpaRepository<User, Long> 
+{
+    Optional<User> findById(Long id);
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByName(String name);
+}
