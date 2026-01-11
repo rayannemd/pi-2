d68e9f0b5ecb2d7f15b89be7a9cd1803b694a6fa
Rayanne Melo Dias <102603196+rayannemd@users.noreply.github.com>
2026-01-11 00:06:07 -0300

    Delete botservice directory

--- botservice/src/main/java/br/ufc/crateus/pi2/botservice/models/enums/EUserType.java
+++
@@ -1,8 +0,0 @@
-package br.ufc.crateus.pi2.botservice.models.enums;
-
-public enum EUserType 
-{
-    CUSTOMER, // Cliente
-    SUPPORT, // Atendente (? - discutir sobre)
-    ADMIN // Administrador
-}
