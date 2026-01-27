fc1de38b9f9e50260157f9220590ebd27dbdff92
NathanSoaresRodrigues <nathansoares39@gmail.com>
2026-01-26 23:29:28 -0300

    refactor: remover importação não utilizada da classe Assert

--- web-api/src/main/java/br/ufc/crateus/pi2/botservice/services/UserService.java
+++ web-api/src/main/java/br/ufc/crateus/pi2/botservice/services/UserService.java
@@ -8,7 +8,6 @@ import br.ufc.crateus.pi2.botservice.controllers.exceptions.DuplicatedResourceEx
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.util.Assert;
 
 import br.ufc.crateus.pi2.botservice.models.User;
 import br.ufc.crateus.pi2.botservice.repositories.ServiceRepository;
