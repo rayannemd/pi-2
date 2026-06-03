3927335a367b526138f3081dd01709c056292c11
JadielSilva11 <jdielcunha@gmail.com>
2026-06-03 15:54:11 -0300

    Revert "Feature/implement collection provider"

--- web-api/src/main/java/br/ufc/crateus/pi2/botservice/BotServiceApplication.java
+++ web-api/src/main/java/br/ufc/crateus/pi2/botservice/BotServiceApplication.java
@@ -2,10 +2,8 @@ package br.ufc.crateus.pi2.botservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
-@ConfigurationPropertiesScan(basePackages = "br.ufc.crateus.pi2.botservice.configs")
 public class BotServiceApplication {
 
 	public static void main(String[] args) {
