1a6493b50de507349265e82f1c80a5bf0abe2c55
mjulianegreiros <xuxuuzinhaa8@gmail.com>
2026-06-08 22:32:44 -0300

    logica de cancelamento de sessao de chat

--- web-api/src/main/java/br/ufc/crateus/pi2/botservice/BotServiceApplication.java
+++ web-api/src/main/java/br/ufc/crateus/pi2/botservice/BotServiceApplication.java
@@ -2,8 +2,10 @@ package br.ufc.crateus.pi2.botservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BotServiceApplication {
 
 	public static void main(String[] args) {
